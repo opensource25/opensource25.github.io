faff762b573617fd401bb0864c656f9c99b77407
opensource25 <b.opensource@gmail.com>
2021-10-25 21:01:20 +0200

    build the note box

--- noten-lernen/main.js
+++ noten-lernen/main.js
@@ -10,7 +10,7 @@ function onload() {
 }
 
 function update_note_amount() {
-  var note_amount = $("#note-amount-range-slider").value;
+  var note_amount = test123
   console.log(note_amount);
   console.log($("#note-amount-range-slider").value);
   $("#note-amount").text(`Noten Anzahl: ${note_amount}`);
